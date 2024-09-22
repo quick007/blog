3714ef23167200e20c780788a2699a84155c6130
Lukas <69743171+quick007@users.noreply.github.com>
2024-09-22 10:53:23 -0700

    use rust based compiler

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,11 @@ import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // experimental: {
-  //   turbo: {
-  //     resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", "md"],
-  //   },
-  // },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  experimental: {
+    // https://nextjs.org/docs/app/building-your-application/configuring/mdx#global-styles-and-components
+    mdxRs: true,
+  },
 };
 
 const withMDX = createMDX({
